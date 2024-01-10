59e557357d79710065e64d5c644ca29be23f095d
vladislav-gh-dump <vladislav.notebook@gmail.com>
2024-01-10 15:47:19 +0300

    add src/cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!');
+import questionName from "../src/cli.js";
+
+
+const app = () => {
+    console.log('Welcome to the Brain Games!');
+
+    questionName();
+};
+
+app();
